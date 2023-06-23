a2d40ea15285ced5a2ed040eb4b8d5aa9358aee4
Purple <jakpere@anchorgroupcorp.com>
2023-06-23 13:51:32 +0100

    Layout set  up
    Landing page set now working on dashboar

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<h1 class="logo"><a class="fw-lighter fs-2" href="/" style="color: #1F2937"> Negosia </a></h1>
---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('dashboard.partials.header')
+
+<body>
+
+@include('dashboard.partials.footer')
+</body>
+
+</html>
